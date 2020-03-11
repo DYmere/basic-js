8203ba9912766b4493565ce475ea05e9b0c8831c
Dzmitry Yatsyna <dyfromby@gmail.com>
2020-03-12 00:15:06 +0300

    feat: add createDreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,9 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members) == false) { return false };
+  let teamName=[];
+  for (name of members) {
+    if (typeof(name) != "string") { continue };
+    teamName.push(name.trim().toUpperCase()[0]);
+  }
+  return teamName.sort().join("");
 };
